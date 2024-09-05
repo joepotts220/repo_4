e1c865f0ae2dc5536d27a65277d377bddd22f93d
Joe Potts <joepotts@Billyes-Air.cinci.rr.com>
2024-09-05 11:19:37 -0400

    add source file

---
+++ vampire.py
@@ -0,0 +1,12 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You ae not Alice.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+spam = 0
+while spam < 6:
+    print('hello world')
+    spam = spam + 1
